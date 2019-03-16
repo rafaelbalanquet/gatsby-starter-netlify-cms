b0ea2577cb1dadbe82eac5dad66dbf05f4465339
Rafael Balanquet <balanquetr@allegheny.edu>
2019-03-16 12:24:35 -0400

    changed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/yuh.svg'
 
 const Navbar = class extends React.Component {
   componentDidMount() {
@@ -37,7 +37,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Tri-Bar" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div className="navbar-burger burger" data-target="navMenu">
