01b598e1d5d36a960a82771b2e27bba67aeb499a
Rafael Balanquet <balanquetr@allegheny.edu>
2019-04-21 14:18:54 -0400

    added blue box and white sides to main page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,8 +39,8 @@ export const IndexPageTemplate = ({
       <h1
         className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
         style={{
-          boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-          backgroundColor: 'rgb(255, 68, 0)',
+          boxShadow: '#FFF 0.5rem 0px 0px, #FFF -0.5rem 0px 0px',
+          backgroundColor: '#000080',
           color: 'white',
           lineHeight: '1',
           padding: '0.25em'
