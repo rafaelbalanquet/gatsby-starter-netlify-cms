13a166080c9b168c8923206b998b8bbff3889dfa
Rafael Balanquet <balanquetr@allegheny.edu>
2019-11-15 13:40:19 -0500

    changed heading homepage

---
+++ src/pages/yeah.py
@@ -0,0 +1,13 @@
+class MyClass: 
+
+    def __init__(self, first_val_int, second_val_int): 
+        self.val1 = first_val_int
+        self.val2 = second_val_int
+    #end of init()
+
+    def sum(self): 
+        print(" The sum of values is: " self.val1 + self.val2)
+    #end of sum()
+#end of MyClass()
+
+x.sum()
